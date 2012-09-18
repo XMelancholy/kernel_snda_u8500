5e7fd6159311f5032ec87d05b313c041572cd53b
Jefferson Delfes <jefferson.delfes@openbossa.org>
2012-09-18 13:36:54 -0400

    Bluetooth: Force the process of unpair command if disconnect failed
    The unpair process tries to disconnect any connection pending with
    remote. If there are some connection in connecting state, disconnect
    command will fail and unpair mgmt command will stay pending.
    
    That pending mgmt command can cause strange behavior like automatic
    unpair after a lost connection.
    
    Signed-off-by: Jefferson Delfes <jefferson.delfes@openbossa.org>
    Acked-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -3146,6 +3146,9 @@ int mgmt_disconnect_failed(struct hci_dev *hdev, bdaddr_t *bdaddr,
 	struct pending_cmd *cmd;
 	int err;
 
+	mgmt_pending_foreach(MGMT_OP_UNPAIR_DEVICE, hdev, unpair_device_rsp,
+			     hdev);
+
 	cmd = mgmt_pending_find(MGMT_OP_DISCONNECT, hdev);
 	if (!cmd)
 		return -ENOENT;
@@ -3158,8 +3161,6 @@ int mgmt_disconnect_failed(struct hci_dev *hdev, bdaddr_t *bdaddr,
 
 	mgmt_pending_remove(cmd);
 
-	mgmt_pending_foreach(MGMT_OP_UNPAIR_DEVICE, hdev, unpair_device_rsp,
-			     hdev);
 	return err;
 }
 
